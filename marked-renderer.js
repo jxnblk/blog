35863b0ffdce08eb10d326409d17d5b57b4317e4
jxnblk <jacksonblack@gmail.com>
2015-06-09 22:34:20 -0400

    Linked headings

--- marked-renderer.js
+++ marked-renderer.js
@@ -1,6 +1,7 @@
 
 var marked = require('marked')
 var highlight = require('highlight.js')
+var _kebab = require('lodash').kebabCase
 
 var renderer = new marked.Renderer()
 
@@ -14,6 +15,20 @@ renderer.code = function (code, lang) {
 
   return result
 
+}
+
+renderer.heading = function (text, level) {
+  var name = _kebab(text);
+  var result;
+  if (level < 4) {
+    result =
+      '<h' + level + ' id="' + name + '">'+
+      '<a href="#' + name + '">'+ text + '</a>'+
+      '</h' + level + '>';
+  } else {
+    result = '<h' + level + '>' + text + '</h' + level + '>';
+  }
+  return result;
 }
 
 module.exports = renderer
