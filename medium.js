20380132fb777ebcd28cfee7a6ec0ed2d3ec58c2
jxnblk <jacksonblack@gmail.com>
2015-10-17 12:38:30 -0400

    Clean up console messages and open browser on success

--- medium.js
+++ medium.js
@@ -4,6 +4,8 @@ import fs from 'fs'
 import path from 'path'
 import medium from 'medium-sdk'
 import frontMatter from 'front-matter'
+import colors from 'colors'
+import open from 'open'
 import config from './config.json'
 
 const { medium: { token: TOKEN } } = config
@@ -31,13 +33,14 @@ try {
 
 const matter = frontMatter(src)
 const { title, tags } = matter.attributes
+const canonicalUrl = `http://jxnblk.com/writing/posts/${slug}`
 
 const content = `
 # ${title}
 
 ${matter.body}
 
-*Cross-posted from [jxnblk.com/writing](http://jxnblk.com/writing/posts/${slug})*
+*Cross-posted from [jxnblk.com/writing](${canonicalUrl})*
 `
 
 client.getUser((err, user) => {
@@ -45,16 +48,25 @@ client.getUser((err, user) => {
     throw new Error(err)
   }
 
-  console.log(user)
+  console.log(`Authenticated as ${user.username}`.blue)
   client.createPost({
     userId: user.id,
     title,
     tags,
     content,
+    canonicalUrl,
     contentFormat: 'markdown',
     publishStatus: 'draft'
   }, (err, post) => {
-    console.log(`Post "${title}" published to Medium.com`, post)
+    if (err) {
+      throw new Error(err)
+    }
+
+    console.log(
+      `Draft post "${title}" published to Medium.com`.green,
+      colors.blue(JSON.stringify(post, null, 2))
+    )
+    open(post.url)
   })
 })
 
