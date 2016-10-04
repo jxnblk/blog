a4cf413d0393b8ae615e6a69faf5aa2f3889ce36
jxnblk <jacksonblack@gmail.com>
2016-10-04 14:20:59 -0400

    Adjust date to string method

--- components/Body.js
+++ components/Body.js
@@ -3,11 +3,20 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 
+const toPrettyDate = created => {
+  return new Date(created).toLocaleString('en-us', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric'
+  })
+}
+
 class Body extends React.Component {
 
   render () {
     const { post, title, children } = this.props
-    const date = post ? new Date(post.created).toDateString() : ''
+    const date = post ? toPrettyDate(post.created) : ''
 
     const view = React.cloneElement(children, this.props)
 
