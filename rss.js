9561cbc1e8e53b0c39574aa93cbee8feb98c0b61
jxnblk <jacksonblack@gmail.com>
2016-10-17 14:54:41 -0400

    Add RSS feed

---
+++ rss.js
@@ -0,0 +1,42 @@
+
+// RSS generator
+
+require('babel-register')()
+const fs = require('fs')
+const Rss = require('rss')
+const data = require('./data')
+
+const {
+  title,
+  description,
+  author,
+} = data
+
+const options = {
+  title,
+  description,
+  site_url: 'http://jxnblk.com/writing',
+  // image_url: 'http://jxnblk.com/avatar/',
+}
+
+const feed = new Rss(options)
+
+console.log('Generating RSS feed for ', data.posts.length)
+
+data.posts.forEach(post => {
+  feed.item({
+    title: post.title,
+    description: post.excerpt,
+    date: post.created,
+    url: `http://jxnblk.com/writing/${post.slug}`,
+    guid: post.slug,
+    categories: post.tags,
+    author
+  })
+})
+
+const xml = feed.xml()
+
+fs.writeFileSync('rss.xml', xml)
+// console.log(xml)
+
