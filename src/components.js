ead363fa6bf54aab959040a971f68427e5ed105f
Brent Jackson <jxnblk@gmail.com>
2018-09-01 14:00:23 -0400

    Remove withRouter from Root

--- src/components.js
+++ src/components.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Head, Link } from 'mdx-go'
-import { withRouter } from 'react-router-dom'
 import RebassMDX from '@rebass/mdx'
 import {
   Flex,
@@ -22,7 +21,7 @@ const Script = ({ src }) =>
 const ga = '(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,"script","//www.google-analytics.com/analytics.js","ga"); ga("create", "UA-4603832-6", "auto"); ga("send", "pageview");'
 
 
-export const Root = withRouter(({
+export const Root = ({
   children,
   location,
   routes,
@@ -78,4 +77,4 @@ export const Root = withRouter(({
       </React.Fragment>
     </RebassMDX>
   )
-})
+}
