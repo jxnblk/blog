4faeb39b00dcfeb85a77bd0ce8a144205657fc70
Brent Jackson <jxnblk@gmail.com>
2019-07-07 01:07:14 +0100

    Adjust theming

--- src/@jxnblk/gatsby-theme-mdx-blog/styles.js
+++
@@ -1,9 +0,0 @@
-import merge from 'lodash.merge'
-import styles from '@jxnblk/gatsby-theme-mdx-blog/src/styles'
-
-export default merge({}, styles, {
-  inlineCode: {
-    bg: 'muted',
-    fontSize: '87.5%',
-  }
-})
