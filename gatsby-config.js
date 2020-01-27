243b07d1afb2dd07b52a107f73c39973e524c7db
Brent Jackson <jxnblk@gmail.com>
2020-01-27 17:59:02 -0500

    Remove theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,18 @@ module.exports = {
     siteUrl: 'https://jxnblk.com'
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        extensions: [
+          '.mdx', '.md',
+        ],
+      }
+    },
+    'gatsby-plugin-theme-ui',
+    'gatsby-plugin-react-helmet',
     'gatsby-plugin-catch-links',
-    'gatsby-theme-jxnblk',
+    // 'gatsby-theme-jxnblk',
     {
       resolve: 'gatsby-plugin-google-fonts',
       options: {
@@ -22,5 +32,9 @@ module.exports = {
         trackingId: 'UA-4603832-6'
       }
     },
+    // {
+    //   resolve: 'gatsby-plugin-feed',
+    //   options: require('./feed')
+    // },
   ]
 }
---
+++ src/pages/blog/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default props =>
+  <pre>
+    blog index TK
+  </pre>
